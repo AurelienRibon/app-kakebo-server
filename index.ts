b13958679b85d22cb4e909827408c02379407c9e
Aurelien Ribon <aurelien.ribon@gmail.com>
2023-09-25 19:42:36 +0200

    Add try/catch around routes

--- index.ts
+++ index.ts
@@ -31,59 +31,74 @@ app.get('/schema', async (req, res) => {
   const logger = new Logger('/schema');
   const dev = !!req.query.dev;
 
-  logger.log('Retrieving schema...');
-  const db = new DB({ dev });
-  const rows = await db.loadSchema();
+  try {
+    logger.log('Retrieving schema...');
+    const db = new DB({ dev });
+    const rows = await db.loadSchema();
 
-  logger.log('Sending response...');
-  res.json(rows);
+    logger.log('Sending response...');
+    res.json(rows);
 
-  logger.log('Done.');
+    logger.log('Done.');
+  } catch (err: any) {
+    logger.error(err);
+    res.status(500).json({ error: err.message });
+  }
 });
 
 app.get('/expenses', async (req, res) => {
   const logger = new Logger('/expenses');
   const dev = !!req.query.dev;
 
-  logger.log('Retrieving expenses...');
-  const db = new DB({ dev });
-  const rows = await db.loadExpenses();
+  try {
+    logger.log('Retrieving expenses...');
+    const db = new DB({ dev });
+    const rows = await db.loadExpenses();
 
-  logger.log('Sending response...');
-  res.json(rows);
+    logger.log('Sending response...');
+    res.json(rows);
 
-  logger.log('Done.');
+    logger.log('Done.');
+  } catch (err: any) {
+    logger.error(err);
+    res.status(500).json({ error: err.message });
+  }
 });
 
 app.post('/expenses/sync', async (req, res) => {
   const logger = new Logger('/expenses/sync');
   const dev = !!req.query.dev;
 
-  logger.log('Loading expenses...');
-  const db = new DB({ dev });
-  const userExpenses = fromUserExpenses(req.body.expenses);
-  const knownExpenses = await db.loadExpenses();
+  try {
+    logger.log('Loading expenses...');
+    const db = new DB({ dev });
+    const userExpenses = fromUserExpenses(req.body.expenses);
+    const knownExpenses = await db.loadExpenses();
 
-  logger.log('Computing diff for server...');
-  const expensesToUpsertForServer = diffExpenses(userExpenses, knownExpenses);
-  logger.log(`Found ${expensesToUpsertForServer.length} new expenses.`);
+    logger.log('Computing diff for server...');
+    const expensesToUpsertForServer = diffExpenses(userExpenses, knownExpenses);
+    logger.log(`Found ${expensesToUpsertForServer.length} new expenses.`);
 
-  logger.log('Computing diff for user...');
-  const expensesToUpsertForUser = diffExpenses(knownExpenses, userExpenses);
-  logger.log(`Found ${expensesToUpsertForUser.length} new expenses.`);
+    logger.log('Computing diff for user...');
+    const expensesToUpsertForUser = diffExpenses(knownExpenses, userExpenses);
+    logger.log(`Found ${expensesToUpsertForUser.length} new expenses.`);
 
-  logger.log('Computing new monthly expenses...');
-  const monthlyExpensesToInsert = generateNewMonthlyExpenses(knownExpenses);
-  logger.log(`Found ${monthlyExpensesToInsert.length} new expenses.`);
+    logger.log('Computing new monthly expenses...');
+    const monthlyExpensesToInsert = generateNewMonthlyExpenses(knownExpenses);
+    logger.log(`Found ${monthlyExpensesToInsert.length} new expenses.`);
 
-  if (expensesToUpsertForServer.length > 0 || monthlyExpensesToInsert.length > 0) {
-    logger.log('Starting upsert...');
-    const expensesToUpsert = [...expensesToUpsertForServer, ...monthlyExpensesToInsert];
-    await db.upsertExpenses(expensesToUpsert);
+    if (expensesToUpsertForServer.length > 0 || monthlyExpensesToInsert.length > 0) {
+      logger.log('Starting upsert...');
+      const expensesToUpsert = [...expensesToUpsertForServer, ...monthlyExpensesToInsert];
+      await db.upsertExpenses(expensesToUpsert);
+    }
+
+    logger.log('Sending response...');
+    res.json({ expenses: [...expensesToUpsertForUser, ...monthlyExpensesToInsert] });
+
+    logger.log('Done.');
+  } catch (err: any) {
+    logger.error(err);
+    res.status(500).json({ error: err.message });
   }
-
-  logger.log('Sending response...');
-  res.json({ expenses: [...expensesToUpsertForUser, ...monthlyExpensesToInsert] });
-
-  logger.log('Done.');
 });
--- lib/logger.ts
+++ lib/logger.ts
@@ -16,4 +16,20 @@ export class Logger {
     this.lastTime = now;
     console.log(`${this.namespace} - ${message} +${durationMs}ms, +${totalDurationMs}ms`);
   }
+
+  error(err: unknown): void {
+    const now = Date.now();
+    const durationMs = now - this.lastTime;
+    const totalDurationMs = now - this.startTime;
+    this.lastTime = now;
+
+    const message = err instanceof Error ? err.message : String(err);
+    const stack = err instanceof Error ? err.stack : '';
+    console.log(`${this.namespace} - Error: ${message} +${durationMs}ms, +${totalDurationMs}ms`);
+    console.error(message);
+
+    if (stack) {
+      console.error(stack);
+    }
+  }
 }
