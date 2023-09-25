91d6d667d9c92d6bb4a9e3d14b3e75c82cc36ef3
Aurelien Ribon <aurelien.ribon@gmail.com>
2023-09-25 19:44:00 +0200

    Fix log of errors

--- lib/logger.ts
+++ lib/logger.ts
@@ -13,6 +13,7 @@ export class Logger {
     const now = Date.now();
     const durationMs = now - this.lastTime;
     const totalDurationMs = now - this.startTime;
+
     this.lastTime = now;
     console.log(`${this.namespace} - ${message} +${durationMs}ms, +${totalDurationMs}ms`);
   }
@@ -21,15 +22,11 @@ export class Logger {
     const now = Date.now();
     const durationMs = now - this.lastTime;
     const totalDurationMs = now - this.startTime;
+
     this.lastTime = now;
+    console.log(`${this.namespace} - Error! +${durationMs}ms, +${totalDurationMs}ms`);
 
-    const message = err instanceof Error ? err.message : String(err);
-    const stack = err instanceof Error ? err.stack : '';
-    console.log(`${this.namespace} - Error: ${message} +${durationMs}ms, +${totalDurationMs}ms`);
-    console.error(message);
-
-    if (stack) {
-      console.error(stack);
-    }
+    const stack = err instanceof Error ? err.stack : String(err);
+    console.error(stack);
   }
 }
